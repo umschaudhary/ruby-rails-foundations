bf20065d095b6006a506d152cbc70c197c240598
umschaudhary <umesschaudhary@gmail.com>
2022-03-29 00:59:09 +0545

    add after save to calculate percent complete of project

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
   belongs_to :project
+  after_save :update_precent_complete if :mark_complete?
+  scope :completed, -> { where(completed: true) }
+
+  def mark_complete?
+    self.completed == true
+  end
+
+  def update_precent_complete
+    project = Project.find(self.project_id)
+    count_of_completed_tasks = project.tasks.completed.count
+    count_of_total_tasks = project.tasks.count
+    project.update!(percent_complete: Counter.calculate_percent_complete(
+      count_of_completed_tasks, count_of_total_tasks))
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_28_185426) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_28_190224) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_28_185426) do
     t.integer "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
