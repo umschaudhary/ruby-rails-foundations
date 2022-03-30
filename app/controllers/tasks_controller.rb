02ea1285d5f1a080a0833ffe20896094541fc491
umschaudhary <umesschaudhary@gmail.com>
2022-03-31 00:18:21 +0545

    add task controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,56 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was created successfully!' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render:new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was updated successfully!' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to project_url(@task.project_id), notice: 'Task was deleted successfully' }
+      format.json { render :show, status: :created, location: @task }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :project_id, :completed, :task_file)
   end
 end
